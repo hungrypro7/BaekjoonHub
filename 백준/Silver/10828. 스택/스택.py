74a3357c0244ac37f1dccb728308ca7bb4575ea3
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-05-04 23:55:59 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,24 @@
+import sys
+N = int(input())
+stack = []
+for i in range(N):
+    input = sys.stdin.readline().split()
+    if input[0] == 'push':
+        stack.append(input[1])
+    elif input[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+    elif input[0] == 'size':
+        print(len(stack))
+    elif input[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif input[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
