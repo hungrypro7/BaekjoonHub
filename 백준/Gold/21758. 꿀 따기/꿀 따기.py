e5381abff45124dcaaa79b7a638d1b40a00dc7b3
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-06-21 00:04:44 +0900

    [Gold V] Title: 꿀 따기, Time: 188 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.py
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.py
@@ -1,6 +1,11 @@
-N = int(input())
-honeys = list(map(int, input().split()))
+import sys
+N = int(sys.stdin.readline())
+honeys = list(map(int, sys.stdin.readline().split()))
 max_list = []
+honeys_sigma = [0]
+for i in range(N):
+    honeys_sigma.append(honeys_sigma[i] + honeys[i])
+honeys_sigma.remove(0)
 
 bee1 = 0        # 첫 번째 시나리오 b h b
 bee2 = N-1
@@ -8,7 +13,7 @@ amount1 = 0
 max1 = 0
 for i in range(1, bee2):
     honey = i
-    amount1 += sum(honeys[bee1+1:honey+1]) + sum(honeys[honey:bee2])
+    amount1 += (honeys_sigma[honey] - honeys_sigma[bee1]) + (honeys_sigma[bee2-1] - honeys_sigma[honey] + honeys[honey])
     if max1 < amount1:
         max1 = amount1
     amount1 = 0
@@ -20,7 +25,7 @@ amount2 = 0
 max2 = 0
 for i in range(bee1+1, honey):
     bee2 = i
-    amount2 += sum(honeys[bee1+1:bee2]) + 2 * sum(honeys[bee2+1:honey+1])
+    amount2 += (honeys_sigma[honey] - honeys_sigma[bee1] - honeys[bee2]) + (honeys_sigma[honey] - honeys_sigma[bee2])
     if max2 < amount2:
         max2 = amount2
     amount2 = 0
@@ -32,7 +37,7 @@ max3 = 0
 amount3 = 0
 for i in range(1, bee2):
     bee1 = i
-    amount3 += 2 * sum(honeys[honey:bee1]) + sum(honeys[bee1+1:bee2])
+    amount3 += honeys_sigma[bee1-1] + (honeys_sigma[bee2-1] - honeys[bee1])
     if max3 < amount3:
         max3 = amount3
     amount3 = 0
