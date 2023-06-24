9efd9d985d6488b6e6fb52975b0ace846facff4a
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-06-24 22:25:19 +0900

    [Gold V] Title: 강의실 배정, Time: 564 ms, Memory: 150692 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -3,18 +3,17 @@ n = int(input())
 time = []
 for i in range(n):
     s, e = map(int, input().split())
-    heapq.heappush(time, [s, e])
+    time.append([s, e])
 
+time.sort()
 room = []
-b = heapq.heappop(time)
-heapq.heappush(room, b[1])
+heapq.heappush(room, time[0][1])
 
 for i in range(1, n):
-    a = heapq.heappop(time)
-    if a[0] < room[0]:
-        heapq.heappush(room, a[1])
+    if time[i][0] < room[0]:
+        heapq.heappush(room, time[i][1])
     else:
         heapq.heappop(room)
-        heapq.heappush(room, a[1])
+        heapq.heappush(room, time[i][1])
 
 print(len(room))
