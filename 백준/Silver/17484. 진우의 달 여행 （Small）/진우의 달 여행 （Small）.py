2931cfd120b3a563388037ab9e72fd824b6ee387
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-07-08 22:38:02 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
@@ -1,28 +1,25 @@
-INF = 1000000
 n, m = map(int, input().split())
-etm = []
-for i in range(n):
-    cost = list(map(int, input().split()))
-    etm.append(cost)
+cost = [list(map(int, input().split())) for _ in range(n)]
 dp = [[[0] * 3 for _ in range(m)] for _ in range(n)]
-for j in range(m):
-    for k in range(3):
-        dp[0][j][k] = etm[0][j]
+
+for i in range(m):
+    for j in range(3):
+        dp[0][i][j] = cost[0][i]
 
 for i in range(1, n):
     for j in range(m):
         for k in range(3):
-            if(j == 0 and k == 0) or (j == m-1 and k == 2):
-                dp[i][j][k] = INF
-                continue
-            if k == 0:
-                dp[i][j][k] = etm[i][j] + min(dp[i-1][j-1][1], dp[i-1][j-1][2])
+            if (j == 0 and k == 0) or (j == m-1 and k == 2):
+                dp[i][j][k] = 1000000
+            elif k == 0:
+                dp[i][j][0] = cost[i][j] + min(dp[i-1][j-1][1], dp[i-1][j-1][2])
             elif k == 1:
-                dp[i][j][k] = etm[i][j] + min(dp[i-1][j][0], dp[i-1][j][2])
-            else:
-                dp[i][j][k] = etm[i][j] + min(dp[i-1][j+1][0], dp[i-1][j+1][1])
+                dp[i][j][1] = cost[i][j] + min(dp[i-1][j][0], dp[i-1][j][2])
+            elif k == 2:
+                dp[i][j][2] = cost[i][j] + min(dp[i-1][j+1][0], dp[i-1][j+1][1])
 
-result = INF
-for j in range(m):
-    result = min(result, min(dp[-1][j]))
+result = 1000000
+for i in dp[-1]:
+    for j in i:
+        result = min(result, j)
 print(result)
