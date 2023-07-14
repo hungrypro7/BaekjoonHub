c677e7f89033ddfbfa578d67f0ec990ad1d75656
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-07-14 17:00:44 +0900

    [Gold IV] Title: 고층 건물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.py
+++ 백준/Gold/1027. 고층 건물/고층 건물.py
@@ -1,24 +1,21 @@
 n = int(input())
-building = list(map(int, input().split()))
-max_see = 0
-for i in range(n):
-    temp_see = 0
-    if n-i >= 2:
-        async1 = building[i+1] - building[i]
-        temp_see += 1
-        for j in range(i+2, n):   # 오른쪽 탐색
-            if 1.0 * (building[j] - building[i]) / (j-i) <= async1:
-                continue
-            async1 = 1.0 * (building[j] - building[i]) / (j-i)
-            temp_see += 1
-    if i > 0:
-        async2 = building[i] - building[i-1]
-        temp_see += 1
+buildings = list(map(int, input().split()))
+ans = 0
+for i, j in enumerate(buildings):       # index, 원소
+    can_see = 0
+    if i < n-1:      # 오른쪽 탐색
+        can_see += 1
+        inclination = buildings[i+1] - j
+        for k in range(i+2, n):
+            if (buildings[k] - j) / (k-i) > inclination:
+                can_see += 1
+            inclination = max((buildings[k] - j) / (k-i), inclination)
+    if i >= 1:
+        can_see += 1
+        inclination = j - buildings[i-1]
         for k in range(i-2, -1, -1):
-            if 1.0 * (building[i] - building[k]) / (i-k) >= async2:
-                continue
-            async2 = 1.0 * (building[k] - building[i]) / (k - i)
-            temp_see += 1
-    max_see = max(max_see, temp_see)
-
-print(max_see)
+            if (j - buildings[k]) / (i-k) < inclination:
+                can_see += 1
+            inclination = min((j - buildings[k]) / (i-k), inclination)
+    ans = max(ans, can_see)
+print(ans)
