03d317a00789af32941a0f5b287e0c016c660dcb
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-07-20 15:03:58 +0900

    [Gold V] Title: 배, Time: 4792 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,26 +1,23 @@
 import sys
 input = sys.stdin.readline
-
-N = int(input())
-cranes = list(map(int, input().split()))
-M = int(input())
-boxes = list(map(int, input().split()))
-
-cranes.sort(reverse=True)
-boxes.sort(reverse=True)
-
-cnt = 0
-
-if boxes[0] > cranes[0]: cnt = -1
-else:
-    while boxes:
-        for c in cranes:
-            if boxes and c < boxes[-1]:
-                continue
-            for b in boxes:
-                if c >= b:
-                    boxes.remove(b)
-                    break
-        cnt+=1
-        
-print(cnt)
+n = int(input())    # 크레인 개수
+crane = list(map(int, input().split()))
+m = int(input())    # 박스 개수
+box = list(map(int, input().split()))
+crane.sort()
+box.sort()
+minute = 0
+if box[-1] > crane[-1]:
+    minute = -1
+    print(minute)
+    sys.exit()
+while box:
+    temp = len(box)-1
+    for i in range(n-1, -1, -1):    # 크레인 탐색
+        for j in range(temp, -1, -1):     # 박스 탐색
+            if box[j] <= crane[i]:
+                box.pop(j)
+                temp = j-1
+                break
+    minute += 1
+print(minute)
