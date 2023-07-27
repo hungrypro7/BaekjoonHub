7bb2da092c0bdbb748fcd12f71436f7733a7f546
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-07-27 22:47:36 +0900

    [Gold IV] Title: 타일 코드, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1720. 타일 코드/타일 코드.py
+++ 백준/Gold/1720. 타일 코드/타일 코드.py
@@ -7,7 +7,7 @@ for i in range(2, n+1):
 if n == 1:
     ans = n
 elif n % 2 == 0:
-    ans = ((fn[n-1]+2*fn[n-2])-(fn[n//2]+2*fn[(n-2)//2]))//2 + (fn[n//2]+2*fn[(n-1)//2])
+    ans = (fn[n]-(fn[n//2]+2*fn[(n-2)//2]))//2 + (fn[n//2]+2*fn[(n-1)//2])
 elif n % 2 == 1:
-    ans = ((fn[n-1]+2*fn[n-2])-(fn[(n-1)//2]))//2 + fn[(n-1)//2]
+    ans = (fn[n]-(fn[(n-1)//2]))//2 + fn[(n-1)//2]
 print(ans)
