48c1e49400eb0fa3256714b91b49393e90fe6056
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-09-26 01:34:41 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+result = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            temp = cards[i] + cards[j] + cards[k]
+            if temp <= m:
+                result = max(result, temp)
+print(result)
