b924c5fc621974c8ac4bec21b2d666f966574f4b
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-10-01 16:58:55 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31256 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+data = []
+medal = []
+for i in range(n):
+    nation, gold, silver, copper = map(int, input().split())
+    data.append((nation, gold, silver, copper))
+    medal.append((gold * 100) + (silver * 10) + (copper * 1))
+medal = sorted(medal, reverse=True)
+temp, ans = 0, 0
+for j in data:
+    if j[0] == k:
+        temp = medal.index(((j[1] * 100) + (j[2] * 10) + (j[3] * 1)))
+        for k in range(temp, -1, -1):
+            if medal[k] > ((j[1] * 100) + (j[2] * 10) + (j[3] * 1)):
+                ans += 1
+        break
+print(ans+1)
