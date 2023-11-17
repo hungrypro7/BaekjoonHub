a90ae0743335711278a9015d381bcd276811b84a
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-11-17 11:57:29 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,27 +1,7 @@
-import sys
+tmp = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 n, b = map(int, input().split())
-exp = 0   # 지수
-temp = 1
-if b == 10:
-    print(n)
-    sys.exit(0)
-elif n < b:
-    if n < 10:
-        print(n)
-    else:
-        print(chr(n+55))
-    sys.exit(0)
-else:
-    while n >= temp:
-        temp *= b
-        exp += 1
-ans = [''] * exp
-for i in range(exp):
-    k = n // (b ** (exp - i - 1))
-    if k >= 10:
-        ans[i] = chr(k+55)
-    else:
-        ans[i] = str(k)
-    n -= (b ** (exp - i - 1)) * k
-for i in ans:
-    print(i, end='')
+ans = ''
+while n != 0:
+    ans += tmp[n % b]
+    n //= b
+print(ans[::-1])
