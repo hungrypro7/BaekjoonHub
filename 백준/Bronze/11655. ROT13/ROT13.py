565737191fefffe812908802893ec2b8fc646b67
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-12-02 12:01:07 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.py
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -1,14 +1,18 @@
 s = input()
+ans = ''
 for i in s:
-    if ord(i) >= 110 and ord(i) <= 122:
-        temp = 122 - ord(i)
-        print(chr(97+(12-temp)), end='')
-    elif ord(i) >= 97 and ord(i) <= 109:
-        print(chr(ord(i)+13), end='')
-    elif ord(i) >= 65 and ord(i) <= 77:
-        print(chr(ord(i) + 13), end='')
-    elif ord(i) >= 78 and ord(i) <= 90:
-        temp = 90 - ord(i)
-        print(chr(65+(12-temp)), end='')
+    if 65 <= ord(i) <= 90:
+        if ord(i) + 13 > 90:
+            temp = (ord(i) + 13) % 90
+            ans += chr(64 + temp)
+        else:
+            ans += chr(ord(i)+13)
+    elif 97 <= ord(i) <= 122:
+        if ord(i) + 13 > 122:
+            temp = (ord(i) + 13) % 122
+            ans += chr(96 + temp)
+        else:
+            ans += chr(ord(i)+13)
     else:
-        print(i, end='')
+        ans += i
+print(ans)
