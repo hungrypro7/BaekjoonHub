e46e59aebac90b6bbdef3ad376a2c85983a442ac
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2023-12-21 09:45:07 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+n = int(input())    # 길이 N
+dp = [[0] * 10 for _ in range(n)]
+for i in range(10):
+    dp[0][i] = 1
+for i in range(1, n):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][j:])
+print(sum(dp[-1])%10007)
