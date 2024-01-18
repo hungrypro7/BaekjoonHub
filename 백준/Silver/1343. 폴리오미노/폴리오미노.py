99868355cb052c2405011d9484a6b05aec7f45e5
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-01-18 12:43:18 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,25 +1,24 @@
-board = list(map(str, input().split('.')))
-state = True
-for i in range(len(board)):
-    valid = len(board[i])
-    str = ''
-    while(valid):
-        if valid >= 4:
-            str += 'AAAA'
-            valid -= 4
-            state = True
-        elif valid >= 2:
-            str += 'BB'
-            valid -= 2
-            state = True
-        else:
-            state = False
-            valid = 0
-    if not state:
-        break
-    board[i] = str
-
-if state:
-    print('.'.join(board))
-else:
-    print(-1)
+import sys
+board = list(input().split('.'))
+ans = ''
+for idx, i in enumerate(board):
+    if len(i) == 0:
+        ans += '.'
+        continue
+    elif len(i) % 2 == 1:
+        print(-1)
+        sys.exit(0)
+    else:
+        p = 0
+        while p < len(i):
+            if len(i) - p >= 4:
+                ans += 'AAAA'
+                p += 4
+            elif len(i) - p >= 2:
+                ans += 'BB'
+                p += 2
+    if idx != len(board) - 1:
+        ans += '.'
+if board[-1] == '':
+    ans = ans[:-1]
+print(ans)
