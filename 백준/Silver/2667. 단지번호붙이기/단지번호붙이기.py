ab5230c06ab262b40c20e010e1a0913694007b6a
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-01-28 20:03:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,31 @@
 import sys
 sys.setrecursionlimit(10**6)
 n = int(input())
-maps = []
+mp = []
 for i in range(n):
-    maps.append(list(input()))
+    mp.append(list(map(int, input())))
 
-def DFS(x, y):
-    global connect
-    connect += 1
-    maps[x][y] = '0'
+def dfs(x, y):
+    global cnt
+    cnt += 1
+    mp[x][y] = 0
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for j in range(4):
+        nx = x + dx[j]
+        ny = y + dy[j]
+        if 0 <= nx < n and 0 <= ny < n and mp[nx][ny]:
+            dfs(nx, ny)
 
-    if x-1 >= 0 and maps[x-1][y] != '0':
-        DFS(x-1, y)
-    if x+1 < n and maps[x+1][y] != '0':
-        DFS(x+1, y)
-    if y-1 >= 0 and maps[x][y-1] != '0':
-        DFS(x, y-1)
-    if y+1 < n and maps[x][y+1] != '0':
-        DFS(x, y+1)
-
-ans = []
 count = 0
+ans = []
 for i in range(n):
     for j in range(n):
-        if maps[i][j] != '0':
-            connect = 0
-            DFS(i, j)
-            ans.append(connect)
+        if mp[i][j] == 1:
+            cnt = 0
+            dfs(i, j)
             count += 1
+            ans.append(cnt)
 
 print(count)
 ans.sort()
