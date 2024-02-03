b54cf1b8e9e2e8f97b92f111c83cd5f3c8ff8e04
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-02-04 00:01:25 +0900

    [Gold V] Title: 토마토, Time: 1352 ms, Memory: 98612 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,33 +1,35 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
-m, n = map(int, input().split())
-tomatos = []
-for i in range(n):
-    tomatos.append(list(map(int, input().split())))
+m, n = map(int, input().split())    # 가로, 세로
+tomato = [list(map(int, input().split())) for _ in range(n)]
 
 queue = deque()
 for i in range(n):
     for j in range(m):
-        if tomatos[i][j] == 1:
+        if tomato[i][j] == 1:
             queue.append((i, j))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and tomatos[nx][ny] == 0:
-            tomatos[nx][ny] = tomatos[x][y] + 1
-            queue.append((nx, ny))
+def bfs():
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < n and 0 <= ny < m and tomato[nx][ny] == 0:
+                tomato[nx][ny] = tomato[x][y] + 1
+                queue.append((nx, ny))
+
+bfs()
 
 ans = 0
 for i in range(n):
     for j in range(m):
-        if tomatos[i][j] == 0:
+        if tomato[i][j] == 0:
             print(-1)
-            sys.exit(0)
-    ans = max(ans, max(tomatos[i]))
+            sys.exit()
+    ans = max(ans, max(tomato[i]))
+
 print(ans-1)
