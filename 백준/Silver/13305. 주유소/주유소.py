f0f54445fb0445fbb977ff46ef03dc3fdbcd09d8
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-02-13 09:49:05 +0900

    [Silver III] Title: 주유소, Time: 200 ms, Memory: 46100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -7,14 +7,19 @@ while(i < N):
     state = False
     if i == N - 1 and a == N - 1:  # i가 마지막일 때
         i = a
+    elif i == N - 1 and a != N - 1:
+        money += costs[i] * distances[i-1]
+
     for j in range(i+1, N):
         if costs[i] >= costs[j] or j == N - 1:
             money += costs[i] * sum(distances[i:j])
             state = True
             a = j
             break
+
     if state:
         i = a
     else:
         i += 1
+
 print(money)
