39a5b08b9fe8c437151018e8d7d5a77f1188f297
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-02-19 23:17:38 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,10 +1,9 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 seq = list(map(int, input().split()))
-dp = [0 for _ in range(n)]
-cnt = 0
-for i in range(n):
-    if i > 0:
-        dp[i] = max(dp[i-1]+seq[i], seq[i])
-    else:
-        dp[i] = seq[i]
+dp = [0] * n
+dp[0] = seq[0]
+for i in range(1, n):
+    dp[i] = max(seq[i]+dp[i-1], seq[i])
 print(max(dp))
