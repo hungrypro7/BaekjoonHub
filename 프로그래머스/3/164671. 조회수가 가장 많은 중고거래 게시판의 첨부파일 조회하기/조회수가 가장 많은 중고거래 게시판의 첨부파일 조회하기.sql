cb7196ac434857286e1d13cefddcb8fe05c43244
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-02-24 00:45:07 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT CONCAT('/home/grep/src/', B.BOARD_ID, '/', B.FILE_ID, B.FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE B
+WHERE B.BOARD_ID = (SELECT BOARD_ID
+                    FROM USED_GOODS_BOARD
+                    ORDER BY VIEWS DESC
+                    LIMIT 1)
+ORDER BY FILE_PATH DESC
