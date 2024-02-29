2eef7dbb42bee7152d69f7ed7e8abc3a21497cb0
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-02-29 15:39:06 +0900

    [Silver III] Title: 숫자 야구, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,36 +1,26 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
-record = []
+inq = []
 for i in range(n):
-    record.append(list(map(str, input().split())))
+  res, st, ba = map(int, input().split())
+  inq.append((str(res), st, ba))
 
-def three_differ(num):
-    a = num[0]
-    b = num[1]
-    c = num[2]
-    if a == '0' or b == '0' or c == '0':
-        return False
-    if a != b and b != c and a != c:
-        return True
-    return False
-
-def dec_s_b(a, b):      # a는 비교하고자 하는 수, b는 123, 356, 327, 489
-    strike, ball = 0, 0
-    for i in range(3):      # 스트라이크 판별
-        if a[i] == b[i]:
-            strike += 1
-        elif a[i] in b:
-            ball += 1
-    return [str(strike), str(ball)]
-
-ans = 0
-count = 0
-for j in range(100, 999):
-    if three_differ(str(j)):
-        for k in range(n):
-            if dec_s_b(str(j), record[k][0]) == record[k][1:3]:
-                count += 1
-        if count == n:
-            ans += 1
-        count = 0
-
-print(ans)
+ans = []
+for i in range(100, 1000):
+    temp = str(i)
+    if temp[0] != temp[1] and temp[1] != temp[2] and temp[0] != temp[2] and (temp[0] != '0' and temp[1] != '0' and temp[2] != '0'):
+        for j in range(n):    # 질문 순회
+            s, b = 0, 0
+            for k in range(3):
+                if inq[j][0][k] == temp[k]:
+                    s += 1
+                elif inq[j][0][k] in temp:
+                    b += 1
+            if s == inq[j][1] and b == inq[j][2]:
+                if j == n-1:
+                    ans.append(temp)
+                continue
+            else:
+                break
+print(len(ans))
