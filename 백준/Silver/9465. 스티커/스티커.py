1d41227573250ae29586e647acd68285a16df12c
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-03-01 01:15:39 +0900

    [Silver I] Title: 스티커, Time: 732 ms, Memory: 40868 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,17 +1,16 @@
-import sys
-input = sys.stdin.readline
-t = int(input())
+t = int(input())    # 테스트 케이스 개수
 for i in range(t):
     n = int(input())
     dp = []
-    for j in range(2):
-        dp.append(list(map(int, input().split())))
+    dp.append(list(map(int, input().split())))
+    dp.append(list(map(int, input().split())))
 
     if n > 1:
         dp[0][1] += dp[1][0]
         dp[1][1] += dp[0][0]
+
     for j in range(2, n):
         dp[0][j] += max(dp[1][j-1], dp[1][j-2])
         dp[1][j] += max(dp[0][j-1], dp[0][j-2])
-
-    print(max(dp[0][-1], dp[1][-1]))
+    ans = max(dp[0][-1], dp[1][-1])
+    print(ans)
