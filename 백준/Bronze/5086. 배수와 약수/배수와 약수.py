f4aaee961a886d082bcfe3934db145baa54d17b0
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-06-02 15:37:43 +0900

    [Bronze III] Title: 배수와 약수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.py
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -1,10 +1,11 @@
 while True:
     a, b = map(int, input().split())
-    if a == 0 and b == 0:
+    if a == 0 and b == 0:    # 종료
         break
-    if a > b and a % b == 0:
-        print('multiple')
-    elif a < b and b % a == 0:
+   
+    if a < b and b % a == 0:
         print('factor')
+    elif a > b and a % b == 0:
+        print('multiple') 
     else:
         print('neither')
