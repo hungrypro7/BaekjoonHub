2582544b51de20935827a72b852b128140d3796c
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2025-12-28 21:42:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,23 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
-dp = []
-while n != 0:
-    if (n >= 5 and n % 5 == 0) or (n-5)%3 == 0:
-        dp.append(5)
-        n -= 5
-    elif (n >= 3 and n % 3 == 0) or n > 10:
-        dp.append(3)
-        n -= 3
-    else:
-        print(-1)
-        sys.exit(0)
-print(len(dp))
+
+cnt = 0
+while n > 0:
+    # 1. n이 5로 나눠질 경우
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+
+    # 2. n이 3으로 나눠질 경우
+    n -= 3
+
+    # 3. n이 5와 3의 조합으로 나눠 담을 수 있는 경우
+    if n == 1 or n == 2:
+        cnt = -1
+        break
+
+    # 4. 5와 3으로 나눠지지 않는 경우
+
+
+    cnt += 1
+
+print(cnt)
