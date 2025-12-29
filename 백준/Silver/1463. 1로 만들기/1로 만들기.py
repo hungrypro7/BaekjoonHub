94a4c83024947463389bcff67776bf1de9bb7b9e
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2025-12-30 00:28:10 +0900

    [Silver III] Title: 1로 만들기, Time: 456 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,23 @@
-x = int(input())
-dp = [0, 0, 1, 1]
-if x <= 3:
-    print(dp[x])
-else:
-    for i in range(4, x+1):
-        if i % 6 == 0:
-            dp.append(min(dp[i//3]+1, dp[i//2]+1, dp[i-1]+1))
-        elif i % 3 == 0:
-            dp.append(min(dp[i//3]+1, dp[i-1]+1))
-        elif i % 2 == 0:
-            dp.append(min(dp[i//2]+1, dp[i-1]+1))
-        else:
-            dp.append(dp[i-1]+1)
-    print(dp[x])
+n = int(input())
+dp = [0] * (n+1)    # dp[i] = i 가 1이 되기 위한 연산 최소 횟수
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 0
+
+    elif i < 4:
+        dp[i] = 1
+
+    elif i % 6 == 0:
+        dp[i] = min(dp[i // 3] + 1, dp[i // 2] + 1, dp[i-1] + 1)
+
+    elif i % 3 == 0:
+        dp[i] = min(dp[i // 3] + 1, dp[i-1] + 1)
+
+    elif i % 2 == 0:
+        dp[i] = min(dp[i // 2] + 1, dp[i-1] + 1)
+
+    else:
+        dp[i] = dp[i-1] + 1
+
+print(dp[n])
